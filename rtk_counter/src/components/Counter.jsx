fa063dede0c5dd58a5c19403ea412a4d90464e97
Sasha Lapina <lapinasasha@gmail.com>
2024-10-08 00:49:50 +0300

    change styles

--- rtk_counter/src/components/Counter.jsx
+++ rtk_counter/src/components/Counter.jsx
@@ -1,16 +1,36 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from '../features/counter/CounterSlice';
+import { useState } from 'react';
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+  const [animationClass, setAnimationClass] = useState('')
+
+  const handleIncrement = () => {
+    setAnimationClass('animate-up')
+    setTimeout(() => {
+      dispatch(increment())
+      setAnimationClass('')
+    }, 300)
+  }
+
+  const handleDecriment = () => {
+    setAnimationClass('animate-down')
+    setTimeout(() => {
+      dispatch(decrement())
+      setAnimationClass('')
+    }, 300)
+  }
 
   return (
-    <div>
+    <div className='counter-container'>
       {' '}
-      <h1>{count}</h1>{' '}
-      <button onClick={() => dispatch(increment())}>Увеличить</button>{' '}
-      <button onClick={() => dispatch(decrement())}>Уменьшить</button>{' '}
+      <h1 className={animationClass}>{count}</h1>{' '}
+      <div className='counter-btns-container'>
+        <button onClick={handleDecriment}>Уменьшить</button>{' '}
+        <button onClick={handleIncrement}>Увеличить</button>{' '}  
+      </div>
     </div>
   );
 };
